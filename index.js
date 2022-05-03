39fd66895d562073d4b1b1ae4356d7d9de222aca
DESKTOP-O709VJ6\Justin <ocamsze@gmail.com>
2022-05-03 18:31:06 -0400

    Testing api request through a website and grabbing certain elements

--- index.js
+++ index.js
@@ -3,12 +3,36 @@ const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
 const res = require('express/lib/response')
+const { contentDisposition } = require('express/lib/utils')
+
 
 const app = express()   //Calling express and saving as app
+
+const articles = []
+
 
 //Listen and if visited, we get response
 app.get('/', (req,res) => {
     res.json('Testing API')
+})
+
+app.get('/news', (req, res) => {
+    axios.get('https://www.theguardian.com/environment/climate-crisis')  //Grabs HTML
+    .then((response) => {
+        const html = response.data
+       const $ = cheerio.load(html) //Allows to pick out elements
+
+       $('a:contains("climate")', html).each(function () {
+           const title = $(this).text()
+           const url = $(this).attr('href')
+
+           articles.push({
+               title,
+               url
+           })
+       })
+       res.json(articles)
+    }).catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log('server running on ' + PORT)) //Backend
