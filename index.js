70a674de4053b8a99d80a80937d861dc6e8da5f0
DESKTOP-O709VJ6\Justin <ocamsze@gmail.com>
2022-05-03 20:26:37 -0400

    Programmed api to read from indeed and fetch a list of internships

--- index.js
+++ index.js
@@ -5,11 +5,45 @@ const cheerio = require('cheerio')
 const res = require('express/lib/response')
 const { contentDisposition } = require('express/lib/utils')
 
+const newspapers =[
+    {
+        name: 'indeed',
+        address: 'https://www.indeed.com/jobs?q=software%20engineer%20intern&from=googlesl&vjk=44a0d1e66ce41202',
+        base: 'indeed.com'
+    },
+    // {
+    //     name: 'glassdoor',
+    //     address: 'https://www.glassdoor.com/Job/software-engineer-intern-jobs-SRCH_KO0,24.htm',
+    //     base: ''
+    // },
+]
 
 const app = express()   //Calling express and saving as app
 
 const articles = []
 
+const jobs = "job "
+var replaced = jobs.split(' ').join('_')
+newspapers.forEach(newspaper => {
+    axios.get(newspaper.address)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html) //Allows to pick out elements
+        $('a:contains("Internship")', html).each(function () {
+            const url = $(this).attr('href')   
+           
+            $('h2:contains("")', html).each(function () {
+            const title = $(this).text() 
+                      
+            articles.push({
+                title,
+                url: newspaper.base + url,
+                source: newspaper.name,
+            })
+        })
+    })
+    })
+})
 
 //Listen and if visited, we get response
 app.get('/', (req,res) => {
@@ -17,22 +51,9 @@ app.get('/', (req,res) => {
 })
 
 app.get('/news', (req, res) => {
-    axios.get('https://www.theguardian.com/environment/climate-crisis')  //Grabs HTML
-    .then((response) => {
-        const html = response.data
-       const $ = cheerio.load(html) //Allows to pick out elements
 
-       $('a:contains("climate")', html).each(function () {
-           const title = $(this).text()
-           const url = $(this).attr('href')
+  res.json(articles)
 
-           articles.push({
-               title,
-               url
-           })
-       })
-       res.json(articles)
-    }).catch((err) => console.log(err))
 })
 
 app.listen(PORT, () => console.log('server running on ' + PORT)) //Backend
